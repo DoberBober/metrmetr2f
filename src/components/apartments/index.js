0a31a0e44c92eb70754eae60142d46d668b5d7a6
DoberBober <pochta.q@bk.ru>
2020-01-26 15:14:41 +0300

    Table skeleton for one level.

--- src/components/apartments/index.js
+++ src/components/apartments/index.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 
-import './apartments.styl'
+import './apartments.styl';
+
+import Hirepurchase from './hirepurchase.svg';
+import Mortgage from './mortgage.svg';
+import Maternal from './maternal.svg';
 
 import { API } from '../../helpers/const';
 
@@ -35,12 +39,66 @@ class Apartments extends Component {
 			return <p>Загрузка...</p>;
 		}
 		return (
-			<article>
-				{this.props.apartments.map((item) => {
-					return(
-						<h1 key={item.id}>{item.title}</h1>
-					)
-				})}
+			<article className="table">
+				<div className="container">
+					<div className="table__list">
+						{/* We need subgrids! */}
+						<section className="table__item table__item--head">
+							<div className="table__cell table__cell--legend">ЖК</div>
+							<div className="table__cell table__cell--legend">Комн</div>
+							<div className="table__cell table__cell--legend">м²</div>
+							<div className="table__cell table__cell--legend">Цена&nbsp;за&nbsp;м², ₽</div>
+							<div className="table__cell table__cell--legend">Стоимость, ₽</div>
+							<div className="table__cell table__cell--legend">Этап</div>
+							<div className="table__cell table__cell--legend">Сдача</div>
+							<div className="table__cell table__cell--legend">Адрес</div>
+							<div className="table__cell table__cell--legend">Способы оплаты</div>
+							<div className="table__cell table__cell--legend">Застройщик</div>
+							<div className="table__cell table__cell--legend">Телефон</div>
+						</section>
+						{this.props.apartments.map((item) => {
+							return(
+								<section className="table__item" key={item.id}>
+									<div className="table__cell">{item.name}</div>
+
+												<div className="table__cell">К1</div>
+												<div className="table__cell">П120</div>
+												<div className="table__cell">Ц20 000</div>
+												<div className="table__cell">С8 000 000</div>
+
+{/*									{item.apartments.map((item) => {
+										return(
+											<React.Fragment key={item.id}>
+												<div className="table__cell">{item.rooms}</div>
+												<div className="table__cell">{item.square}</div>
+												<div className="table__cell">{item.price}</div>
+												<div className="table__cell">{item.cost}</div>
+											</React.Fragment>
+										)
+									})}*/}
+									<div className="table__cell">{item.stage}</div>
+									<div className="table__cell">{item.completion}</div>
+									<div className="table__cell">{item.district}<br />{item.address}</div>
+									<div className="table__cell table__cell--options">
+										{item.hirepurchase &&
+											<Hirepurchase width="25" aria-label="Рассрочка" title="Рассрочка" />
+										}
+										{item.mortgage &&
+											<Mortgage width="25" aria-label="Ипотека" title="Ипотека" />
+										}
+										{item.maternalcapital &&
+											<Maternal width="25" aria-label="Материнский капитал" title="Материнский капитал" />
+										}
+									</div>
+									<div className="table__cell">{item.company}</div>
+									<div className="table__cell">
+										<a href="tel:88002000600">8(800)200-06-00</a>
+									</div>
+								</section>
+							)
+						})}
+					</div>
+				</div>
 			</article>
 		)
 	}
