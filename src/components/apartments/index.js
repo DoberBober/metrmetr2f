f3eec2b6b74f985bfe577115344aa268c6361e4d
DoberBober <pochta.q@bk.ru>
2020-01-31 11:44:26 +0300

    Filter logic.

--- src/components/apartments/index.js
+++ src/components/apartments/index.js
@@ -17,6 +17,7 @@ import { fetchApartments } from './actions.js';
 const mapStateToProps = (state) => {
 	return {
 		apartments: state.apartments,
+		filterState: state.filterState,
 		hasError: state.hasError,
 		isLoading: state.isLoading
 	}
@@ -33,26 +34,7 @@ class Apartments extends Component {
 		this.props.fetchData(API + 'all')
 	}
 	apartmentClick(evt){
-		if(!evt.currentTarget.classList.contains('table__item--open')){
-			let anotherApartments = document.querySelectorAll('.table__item--visible')
-
-			if(anotherApartments.length){
-				document.querySelector('.table__item--open').classList.remove('table__item--open')
-				for(let i=0; i<anotherApartments.length; i++){
-					anotherApartments[i].classList.remove('table__item--visible')
-					anotherApartments[i].classList.add('table__item--hidden')
-				}
-			}
-		}
-
-		let apartments = document.querySelectorAll(`.table__item[data-apartmentname="${evt.currentTarget.dataset.apartmentname}"]:not(.table__item--main)`)
-		if(apartments.length){
-			evt.currentTarget.classList.toggle('table__item--open')
-			for(let i=0; i<apartments.length; i++){
-				apartments[i].classList.toggle('table__item--hidden')
-				apartments[i].classList.toggle('table__item--visible')
-			}
-		}
+		evt.currentTarget.classList.toggle('table__item--open')
 	}
 	render() {
 		if (this.props.hasError) {
@@ -84,66 +66,77 @@ class Apartments extends Component {
 						{!this.props.apartments.length &&
 							<Empty />
 						}
-						{this.props.apartments.map((item) => {
-							return item.apartments.map((apartment) => {
-								return(
-									<React.Fragment key={apartment.id}>
-										<section
-											className={apartment.floors.length ? "table__item table__item--main table__item--spoiler" : "table__item table__item--main"}
-											data-apartmentname={item.slug + '-' + apartment.id}
-											onClick={apartment.floors.length ? this.apartmentClick : null}
-											onKeyDown={apartment.floors.length ? (evt) => { evt.keyCode == 13 ? this.apartmentClick(evt) : null } : null}
-											tabIndex={apartment.floors.length ? "0" : ""}
-										>
-											<div className="table__cell">{item.name}</div>
-
-											<div className="table__cell">{apartment.rooms}</div>
-											<div className="table__cell">
-												{
-
-													(apartment.floors.length ? apartment.floors[0]['floor'] : '')
-													+
-													(apartment.floors.length>1 ? ('...' + apartment.floors[apartment.floors.length-1]['floor']) : '')
-
-												}
-											</div>
-											<div className="table__cell">{apartment.square}</div>
-											<div className="table__cell">{apartment.floors.length ? ('от ' + apartment.price) : apartment.price}</div>
-											<div className="table__cell">{apartment.floors.length ? ('от ' + apartment.cost) : apartment.cost}</div>
-
-											<div className="table__cell">{item.stage}</div>
-											<div className="table__cell">{item.completion}</div>
-											<div className="table__cell">{item.district}<br />{item.address}</div>
-											<div className="table__cell table__cell--options">
-												{item.hirepurchase &&
-													<Hirepurchase width="25" aria-label="Рассрочка" title="Рассрочка" />
-												}
-												{item.mortgage &&
-													<Mortgage width="25" aria-label="Ипотека" title="Ипотека" />
-												}
-												{item.maternalcapital &&
-													<Maternal width="25" aria-label="Материнский капитал" title="Материнский капитал" />
-												}
-											</div>
-											<div className="table__cell">{item.company}</div>
-											<div className="table__cell">
-												<a href={'tel:' + item.phone.replace(/[-()\s]/g, '')}>{item.phone}</a>
-											</div>
-										</section>
-										{apartment.floors.map((floor, index) => {
-											return(
+						{/* Опа, говнокод! */}
+						{this.props.apartments
+							.filter((apartment) => {
+								if(this.props.filterState.stage){
+									return apartment.stage == this.props.filterState.stage
+								} else {
+									return apartment
+								}
+							})
+							.filter((apartment) => {
+								if(this.props.filterState.house){
+									return apartment.name == this.props.filterState.house
+								} else {
+									return apartment
+								}
+							})
+							.filter((apartment) => {
+								if(this.props.filterState.district){
+									return apartment.district == this.props.filterState.district
+								} else {
+									return apartment
+								}
+							})
+							.map((item) => {
+								return item.apartments
+									.filter((apartment) => {
+										if(this.props.filterState.minPrice){
+											return apartment.cost >= this.props.filterState.minPrice
+										} else {
+											return apartment
+										}
+									})
+									.filter((apartment) => {
+										if(this.props.filterState.maxPrice){
+											return apartment.cost <= this.props.filterState.maxPrice
+										} else {
+											return apartment
+										}
+									})
+									.filter((apartment) => {
+										if(this.props.filterState.rooms && this.props.filterState.rooms.length){
+											return this.props.filterState.rooms.indexOf(String(apartment.rooms)) > -1
+										} else {
+											return apartment
+										}
+									})
+									.map((apartment) => {
+										return(
+											<section className="table__group" key={apartment.id}>
 												<section
-													className="table__item table__item--hidden"
-													key={floor.id}
+													className={apartment.floors.length ? "table__item table__item--main table__item--spoiler" : "table__item table__item--main"}
 													data-apartmentname={item.slug + '-' + apartment.id}
+													onClick={apartment.floors.length ? this.apartmentClick : null}
+													onKeyDown={apartment.floors.length ? (evt) => { evt.keyCode == 13 ? this.apartmentClick(evt) : null } : null}
+													tabIndex={apartment.floors.length ? "0" : ""}
 												>
 													<div className="table__cell">{item.name}</div>
 
 													<div className="table__cell">{apartment.rooms}</div>
-													<div className="table__cell">{floor.floor}</div>
+													<div className="table__cell">
+														{
+
+															(apartment.floors.length ? apartment.floors[0]['floor'] : '')
+															+
+															(apartment.floors.length>1 ? ('...' + apartment.floors[apartment.floors.length-1]['floor']) : '')
+
+														}
+													</div>
 													<div className="table__cell">{apartment.square}</div>
-													<div className="table__cell">{floor.price}</div>
-													<div className="table__cell">{floor.cost}</div>
+													<div className="table__cell">{apartment.floors.length ? ('от ' + apartment.price) : apartment.price}</div>
+													<div className="table__cell">{apartment.floors.length ? ('от ' + apartment.cost) : apartment.cost}</div>
 
 													<div className="table__cell">{item.stage}</div>
 													<div className="table__cell">{item.completion}</div>
@@ -164,12 +157,56 @@ class Apartments extends Component {
 														<a href={'tel:' + item.phone.replace(/[-()\s]/g, '')}>{item.phone}</a>
 													</div>
 												</section>
-											)
-										})}
-									</React.Fragment>
-								)
+												{apartment.floors
+													.filter((floor) => {
+														if(this.props.filterState.maxPrice){
+															return floor.cost <= this.props.filterState.maxPrice
+														} else {
+															return floor
+														}
+													})
+													.map((floor, index) => {
+														return(
+															<section
+																className="table__item table__item--options"
+																key={floor.id}
+																data-apartmentname={item.slug + '-' + apartment.id}
+															>
+																<div className="table__cell">{item.name}</div>
+
+																<div className="table__cell">{apartment.rooms}</div>
+																<div className="table__cell">{floor.floor}</div>
+																<div className="table__cell">{apartment.square}</div>
+																<div className="table__cell">{floor.price}</div>
+																<div className="table__cell">{floor.cost}</div>
+
+																<div className="table__cell">{item.stage}</div>
+																<div className="table__cell">{item.completion}</div>
+																<div className="table__cell">{item.district}<br />{item.address}</div>
+																<div className="table__cell table__cell--options">
+																	{item.hirepurchase &&
+																		<Hirepurchase width="25" aria-label="Рассрочка" title="Рассрочка" />
+																	}
+																	{item.mortgage &&
+																		<Mortgage width="25" aria-label="Ипотека" title="Ипотека" />
+																	}
+																	{item.maternalcapital &&
+																		<Maternal width="25" aria-label="Материнский капитал" title="Материнский капитал" />
+																	}
+																</div>
+																<div className="table__cell">{item.company}</div>
+																<div className="table__cell">
+																	<a href={'tel:' + item.phone.replace(/[-()\s]/g, '')}>{item.phone}</a>
+																</div>
+															</section>
+														)
+													})
+												}
+											</section>
+										)
+									})
 							})
-						})}
+						}
 					</div>
 				</div>
 			</article>
